0a79778f61b099fd422aabfbbfc8b6e324d8b45c
huyennm-1670 <maddie.nguyen0908@gmail.com>
2021-11-29 15:45:55 +0700

    get all relevant tables

--- create_connection.py
+++ create_connection.py
@@ -1,18 +1,29 @@
-import pandas as pd
 from cassandra.io.libevreactor import LibevConnection
 from cassandra.cluster import Cluster
+import pandas as pd
 
 cluster = Cluster(connect_timeout=30)
 cluster.connection_class = LibevConnection
 session = cluster.connect()
-session.set_keyspace("v10_stg")
 
 # keyspace: v10_stg for staging and v10_prod for prod
-session.set_keyspace("v10_stg")
+session.set_keyspace("v10_prod")
 
-query = "SELECT * FROM users LIMIT 10"
-rows = session.execute(query).all()
+tables = [
+    "accounts",
+    "users",
+    "parties",
+    "user_sessions",
+    "party_user",
+    "blocks",
+    "block_user",
+]
 
-df = pd.DataFrame(rows)
+for table in tables:
+    query = f"SELECT * FROM {table}"
+    rows = session.execute(query).all()
+    df = pd.DataFrame(rows)
+    # display(df)
+    df.to_csv(f"{table}.csv", encoding="utf-8")
+
 cluster.shutdown()
-df.to_csv("users.csv", encoding="utf-8")
